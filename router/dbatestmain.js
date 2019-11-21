e5128098c9cc474d10f5e2c9db5e66264c63a9e8
epicarts <0505zxc@gmail.com>
2019-11-21 15:08:05 +0900

    upload

--- router/dbatestmain.js
+++ router/dbatestmain.js
@@ -11,8 +11,8 @@ var multer = require('multer');// 파일 업로드용 미들웨어
 var fs = require('fs');
 var cors = require('cors');//클라이언트에서 ajax로 요청 시 CORS(다중 서버 접속) 지원
 var MongoClient = require('mongodb').MongoClient;
+//var mongoose = require('')
 
-/////create
 var app = express();
 
 
@@ -67,6 +67,7 @@ var authuser = function(database, id, pw, callback){//꼮꼮 공부하기 callba
     console.log("authuser함수")
     var users = database.collection('users'); //database
     
+    //users.find 에서 데이터 베이스로 검색을 함.
     users.find({'id':id, 'password':pw}).toArray(function(err, docs){//toArray로 배열을 만들때 생기는 err, docs반환하는 콜백함수....ㅠ
         if(err){
             throw err;
@@ -81,6 +82,53 @@ var authuser = function(database, id, pw, callback){//꼮꼮 공부하기 callba
         }
     });
 }; 
+
+var adduser = function(database, id, pw, name, callback){
+    console.log("add user 함수 시작");
+    var users = database.collection('users');
+    
+    users.insertMany([{"id":id, "password":pw, "name":name}], function(err, result){
+        if(err){
+            callback(err, null); // 에러가 나오면 콜백 해쥼
+            return;
+        }
+        if(result.insertedCount > 0){//하나라도 추가가 됫으면,
+            console.log('사용자 추가 완료');
+            callback(err, result);
+        }
+        else{
+            console.log('사용자 추가 실패..');
+            callback(err, null);
+        }
+    });// 여러개의 데이터를 집어 넣을 수 있음.
+};
+
+//adduser가 post 로 접근해 오면.
+router.route('/process/adduser').post(function(req,res){
+    console.log("precess adduser success");
+    var id = req.body.id;
+    var pw = req.body.pw;
+    var name = req.body.name
+    if (database){
+        adduser(database, id, pw, name, function(err, result){//콜백
+            if (err) throw err;
+            if (result && result.insertedCount > 0){//
+                console.dir(result);
+                res.writeHead('200', {'Content-Type':'text/html;charset=utf8'});
+                res.write("<h2>사용자 추가 성공...</h2>");
+                res.end();
+            } else{
+                res.writeHead('200', {'Content-Type':'text/html;charset=utf8'});
+                res.write("<h2>사용자 추가 실패...</h2>");
+                res.end();
+            }
+        });
+    }else{
+        res.writeHead('200', {'Content-Type':'text/html;charset=utf8'});
+        res.write("<h2>데이터베이스 연결 실패...</h2>");
+        res.end();
+    }
+});
 
 router.route('/process/login').post(function(req, res){
     var id = req.body.id;
